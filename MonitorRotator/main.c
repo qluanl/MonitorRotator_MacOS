d435fc217380ca08b07b8ccd90454d620ec64ccf
Shenggao Li <qluanl.sg@gmail.com>
2022-07-12 09:52:54 +0800

    Initial Commit

---
+++ MonitorRotator/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MonitorRotator
+//
+//  Created by 李晟皋 on 2022/7/12.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
